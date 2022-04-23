ca8b7a0cd52a66e000a49c65e45c273938e1bb04
Lizzy Wiley <Lizzyrho@gmail.com>
2022-04-23 14:27:06 -0500

    connected to database

--- server/app.js
+++ server/app.js
@@ -7,10 +7,26 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('We are up and running!');
+
+// mongoose connection
+const mongoose = require('mongoose'); // pull in mongoose 
+mongoose.connect(process.env.MONGODB_URL); // conenct to mongodb using mongoose
+const db = mongoose.connection; 
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', _ => {
+  console.log('We\'re connected!');
+});
+
+
+// This is the route Jerrod can call to practice with react
+app.get('/home', (req, res) => {
+    res.send('We are up and running!');
 })
 
+// app.get('/allbracelets', (req, res) => {
+//  // Code our braclet data here
+// })
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)  
+  console.log(`Example app listening on port ${PORT}`);  
 })
